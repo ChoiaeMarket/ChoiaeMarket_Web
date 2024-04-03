6c523cef449a51d82b54921be48b266d97d18b80
nodb <scnoh02@naver.com>
2024-04-03 20:00:46 +0900

    update: 평균거래가격 계산

--- src/routes/product.tsx
+++ src/routes/product.tsx
@@ -141,13 +141,13 @@ const Dropdown = styled.div`
 const ProdoctList = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 20px;
 `;
 
 const Prodoct = styled.li`
   width: 390px;
   height: 140px;
-  padding: 32px 20px;
+  padding: 20px 32px;
+  border-bottom: 1px solid #252932;
   display: flex;
   cursor: pointer;
   overflow: hidden;
@@ -200,6 +200,12 @@ const ProductDate = styled.div`
   color: #777c89;
 `;
 
+const ProductInfos = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
 const ProductInfo = styled.div`
   font-weight: 400;
   font-size: 12px;
@@ -242,6 +248,34 @@ export function Product() {
       idol: "NCT",
       type: "MD",
       name: "OFFICIAL FANLIGHT",
+      title: "시즈니 필수템 판매중",
+      content: "필요하시면 채팅주세요!",
+      id: "NNNCCT",
+      date: "2024-04-03T19:15:50",
+      price: 30000,
+      chats: 0,
+      likes: 2,
+      order: 5,
+      sold: false,
+    },
+    {
+      idol: "NCT",
+      type: "MD",
+      name: "OFFICIAL FANLIGHT",
+      title: "응원봉 판매",
+      content: "응원봉 싸게 팝니당. 편하게 채팅해주세요.",
+      id: "NNNCCT",
+      date: "2024-04-03T19:15:50",
+      price: 28000,
+      chats: 1,
+      likes: 4,
+      order: 4,
+      sold: false,
+    },
+    {
+      idol: "NCT",
+      type: "MD",
+      name: "OFFICIAL FANLIGHT",
       title: "NCT 응원봉",
       content: "채팅 부탁드려요.",
       id: "NNNCCT",
@@ -250,6 +284,7 @@ export function Product() {
       chats: 1,
       likes: 7,
       order: 3,
+      sold: false,
     },
     {
       idol: "NCT",
@@ -263,6 +298,7 @@ export function Product() {
       chats: 0,
       likes: 1,
       order: 2,
+      sold: false,
     },
     {
       idol: "NCT",
@@ -273,10 +309,11 @@ export function Product() {
         "딱 한 번 썼어요! 하자 전혀 없구요 배터리, 상자, 더스트백 다 포함해서 수요일에 배송 보내드려요~",
       id: "홍튜브",
       date: "2024-03-29T12:28:50",
-      price: 25000,
+      price: 29000,
       chats: 2,
       likes: 13,
       order: 1,
+      sold: false,
     },
   ]);
   const [sortedProducts, setSortedProducts] = useState<any>([
@@ -285,6 +322,34 @@ export function Product() {
       idol: "NCT",
       type: "MD",
       name: "OFFICIAL FANLIGHT",
+      title: "시즈니 필수템 판매중",
+      content: "필요하시면 채팅주세요!",
+      id: "NNNCCT",
+      date: "2024-04-03T19:15:50",
+      price: 30000,
+      chats: 0,
+      likes: 2,
+      order: 5,
+      sold: false,
+    },
+    {
+      idol: "NCT",
+      type: "MD",
+      name: "OFFICIAL FANLIGHT",
+      title: "응원봉 판매",
+      content: "응원봉 싸게 팝니당. 편하게 채팅해주세요.",
+      id: "NNNCCT",
+      date: "2024-04-03T19:15:50",
+      price: 28000,
+      chats: 1,
+      likes: 4,
+      order: 4,
+      sold: false,
+    },
+    {
+      idol: "NCT",
+      type: "MD",
+      name: "OFFICIAL FANLIGHT",
       title: "NCT 응원봉",
       content: "채팅 부탁드려요.",
       id: "NNNCCT",
@@ -293,6 +358,7 @@ export function Product() {
       chats: 1,
       likes: 7,
       order: 3,
+      sold: false,
     },
     {
       idol: "NCT",
@@ -306,6 +372,7 @@ export function Product() {
       chats: 0,
       likes: 1,
       order: 2,
+      sold: false,
     },
     {
       idol: "NCT",
@@ -316,20 +383,25 @@ export function Product() {
         "딱 한 번 썼어요! 하자 전혀 없구요 배터리, 상자, 더스트백 다 포함해서 수요일에 배송 보내드려요~",
       id: "홍튜브",
       date: "2024-03-29T12:28:50",
-      price: 25000,
+      price: 29000,
       chats: 2,
       likes: 13,
       order: 1,
+      sold: false,
     },
   ]); // 정렬된 상품 목록
+
+  const averagePrice = // 평균거래가격
+    products.reduce((total: any, product: any) => total + product.price, 0) /
+    products.length;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`your-api-url`);
         const data = await response.json();
-        setProducts(data);
-        setSortedProducts(data);
+        setProducts(data.filter((product: any) => !product.sold)); // sold가 false인 제품만 필터링
+        setSortedProducts(data.filter((product: any) => !product.sold)); // sold가 false인 제품만 필터링
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -468,8 +540,8 @@ export function Product() {
         />
         <CoverContentsBox>
           <CoverContents>
-            <CoverContentTitle>평균거래내역</CoverContentTitle>
-            <CoverContent>30000원</CoverContent>
+            <CoverContentTitle>평균거래가격</CoverContentTitle>
+            <CoverContent>{averagePrice.toLocaleString()}원</CoverContent>
           </CoverContents>
           <CoverContents>
             <CoverContentTitle>거래내역</CoverContentTitle>
@@ -515,64 +587,74 @@ export function Product() {
         </Dropdown>
       </DropdownContent>
       <ProdoctList>
-        {sortedProducts.map((item: any, index: number) => (
-          <Prodoct
-            key={index}
-            onClick={() => handleProductClick(item.order)} // 클릭 시 상세 정보 페이지로 이동
-          >
-            <ProductImg
-              src={`/src/assets/idol/product/${idol}/${item.name}/${item.order}.png`}
-              alt={item.name}
-              onError={(e) => {
-                (
-                  e.target as HTMLImageElement
-                ).src = `/src/assets/idol/logo/default.png`; // 대체 이미지 설정
-              }}
-            />{" "}
-            <div>
-              <ProductPrice>{item.price.toLocaleString()}원</ProductPrice>{" "}
-              {/* 가격을 세 자리 단위로 끊어서 출력 */}
-              <ProductTitle>{item.title}</ProductTitle>
-              <ProductBox>
-                <ProductDate>
-                  {getTimeDifferenceString(new Date(item.date))}
-                </ProductDate>
-                <ProductInfo>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                  >
-                    <path
-                      d="M9.00002 14.2445H8.76817C7.93639 14.2063 7.12101 13.9675 6.38726 13.5443C6.38678 13.5441 6.3863 13.5438 6.38581 13.5435L6.19234 13.4307L5.92239 13.2733L5.62054 13.3541L4.28986 13.7105L4.64626 12.3809L4.72724 12.0788L4.56959 11.8087L4.45575 11.6136C4.45565 11.6135 4.45554 11.6133 4.45544 11.6131C3.99308 10.8172 3.75002 9.91155 3.75002 8.99727C3.75002 6.1024 6.103 3.75 9.00002 3.75C11.897 3.75 14.25 6.1024 14.25 8.99727C14.25 11.8921 11.897 14.2445 9.00002 14.2445Z"
-                      stroke="#9EA3B2"
-                      stroke-width="1.5"
-                    />
-                  </svg>
-                  {item.chats}{" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                  >
-                    <path
-                      d="M9.00001 14.4001L3.72458 9.33643C2.35847 8.02445 2.35847 5.89606 3.72458 4.58408C5.09068 3.2721 7.30894 3.2721 8.67505 4.58408L9.00001 4.8954L9.32495 4.58408C10.6911 3.2721 12.9093 3.2721 14.2754 4.58408C15.6415 5.89606 15.6415 8.02445 14.2754 9.33643L9.00001 14.4001Z"
-                      stroke="#9EA3B2"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                  {item.likes}
-                </ProductInfo>
-              </ProductBox>
-            </div>
-          </Prodoct>
-        ))}
+        {sortedProducts
+          .filter((product: any) => !product.sold) // sold가 false인 제품만 필터링
+          .map((item: any, index: number) => (
+            <Prodoct
+              key={index}
+              onClick={() => handleProductClick(item.order)} // 클릭 시 상세 정보 페이지로 이동
+            >
+              <ProductImg
+                src={`/src/assets/idol/product/${idol}/${item.name}/${item.order}.png`}
+                alt={item.name}
+                onError={(e) => {
+                  (
+                    e.target as HTMLImageElement
+                  ).src = `/src/assets/idol/logo/default.png`; // 대체 이미지 설정
+                }}
+              />{" "}
+              <div>
+                <ProductPrice>{item.price.toLocaleString()}원</ProductPrice>{" "}
+                {/* 가격을 세 자리 단위로 끊어서 출력 */}
+                <ProductTitle>{item.title}</ProductTitle>
+                <ProductBox>
+                  <ProductDate>
+                    {getTimeDifferenceString(new Date(item.date))}
+                  </ProductDate>
+                  <ProductInfos>
+                    {item.chats !== 0 && ( // item.chats가 0이 아닌 경우에만 렌더링
+                      <ProductInfo>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                        >
+                          <path
+                            d="M9.00002 14.2445H8.76817C7.93639 14.2063 7.12101 13.9675 6.38726 13.5443C6.38678 13.5441 6.3863 13.5438 6.38581 13.5435L6.19234 13.4307L5.92239 13.2733L5.62054 13.3541L4.28986 13.7105L4.64626 12.3809L4.72724 12.0788L4.56959 11.8087L4.45575 11.6136C4.45565 11.6135 4.45554 11.6133 4.45544 11.6131C3.99308 10.8172 3.75002 9.91155 3.75002 8.99727C3.75002 6.1024 6.103 3.75 9.00002 3.75C11.897 3.75 14.25 6.1024 14.25 8.99727C14.25 11.8921 11.897 14.2445 9.00002 14.2445Z"
+                            stroke="#9EA3B2"
+                            stroke-width="1.5"
+                          />
+                        </svg>
+                        {item.chats}
+                      </ProductInfo>
+                    )}
+                    {item.likes !== 0 && ( // item.likes가 0이 아닌 경우에만 렌더링
+                      <ProductInfo>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                        >
+                          <path
+                            d="M9.00001 14.4001L3.72458 9.33643C2.35847 8.02445 2.35847 5.89606 3.72458 4.58408C5.09068 3.2721 7.30894 3.2721 8.67505 4.58408L9.00001 4.8954L9.32495 4.58408C10.6911 3.2721 12.9093 3.2721 14.2754 4.58408C15.6415 5.89606 15.6415 8.02445 14.2754 9.33643L9.00001 14.4001Z"
+                            stroke="#9EA3B2"
+                            stroke-width="1.5"
+                            stroke-miterlimit="10"
+                            stroke-linejoin="round"
+                          />
+                        </svg>
+                        {item.likes}
+                      </ProductInfo>
+                    )}
+                  </ProductInfos>
+                </ProductBox>
+              </div>
+            </Prodoct>
+          ))}
       </ProdoctList>
     </Wrapper>
   );

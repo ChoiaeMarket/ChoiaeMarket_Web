5e343b4d6e2a950372d6d26f3263a2e7ac1310fb
nodb <scnoh02@naver.com>
2024-05-15 18:00:56 +0900

    update: 쿠키의 token으로 로그인 여부 확인

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,16 +1,32 @@
 import { Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
 
 export default function ProtectedRoute({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  //   const user = isUserLogin; // 유저의 로그인 여부(User | null)
-  const user = "abc";
-  // const user = null;
-  console.log("user: ", user);
+  const [isLogin, setIsLogin] = useState<boolean | null>(null);
+  const [cookies] = useCookies(["accessToken"]);
 
-  if (user === null) {
+  useEffect(() => {
+    const token = cookies.accessToken;
+    console.log(token);
+    if (token) {
+      // 토큰 유효성을 검사하는 로직 추가
+      // 예를 들어, 토큰 만료 여부를 확인하는 API 호출
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, [cookies]);
+
+  if (isLogin === null) {
+    return <div>Loading...</div>; // 로딩 중일 때 보여줄 컴포넌트
+  }
+
+  if (isLogin === false) {
     return <Navigate to="/login" />;
   }
   return children;

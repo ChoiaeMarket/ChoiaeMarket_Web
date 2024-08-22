2612ed7fadba1ba7411cd78a6b2e9ea4aca3aac3
nodb <scnoh02@naver.com>
2024-08-22 09:59:34 +0900

    add: 게시물 수정페이지 patch api 연동

---
+++ src/apis/request/board/patch-board.request.dto.ts
@@ -0,0 +1,9 @@
+export default interface PatchBoardRequestDto {
+  idol: string;
+  type: string;
+  name: string;
+  boardImageList: string[];
+  title: string;
+  content: string;
+  price: number;
+}

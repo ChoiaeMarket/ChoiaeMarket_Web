0cad3ffc8553f3c26c274ed0ea1411e46197b9a8
nodb <scnoh02@naver.com>
2024-09-26 13:33:09 +0900

    refactor: 코드 위치 수정

--- src/apis/response/board/index.ts
+++ src/apis/response/board/index.ts
@@ -4,17 +4,19 @@ import GetFavoriteBoardListResponseDto from "./get-favorite-board-list.response.
 import GetFavoriteResponseDto from "./get-favorite.response.dto";
 import GetBoardListResponseDto from "./get-board-list.response.dto";
 import GetSearchBoardListResponseDto from "./get-search-board-list.response.dto";
+import GetUserBoardListResponseDto from "./get-user-board-list.response.dto.ts";
 import PatchBoardResponseDto from "./patch-board.response.dto";
 import PostBoardResponseDto from "./post-board.response.dto";
 import PutFavoriteResponseDto from "./put-favorite.response.dto";
 
 export type {
   DeleteBoardResponseDto,
+  GetBoardListResponseDto,
   GetBoardResponseDto,
   GetFavoriteBoardListResponseDto,
   GetFavoriteResponseDto,
-  GetBoardListResponseDto,
   GetSearchBoardListResponseDto,
+  GetUserBoardListResponseDto,
   PatchBoardResponseDto,
   PostBoardResponseDto,
   PutFavoriteResponseDto,

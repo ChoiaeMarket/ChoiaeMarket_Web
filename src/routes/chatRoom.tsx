4cb8010e5433917b82213df5ea2ed2b10a8f5bb3
nodb <scnoh02@naver.com>
2024-10-25 16:52:36 +0900

    update: 웹소켓 오류 수정

--- src/routes/chatRoom.tsx
+++ src/routes/chatRoom.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { Stomp } from "@stomp/stompjs";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SockJS from "sockjs-client";
-import * as Stomp from "stompjs";
 
 // 채팅 메시지 인터페이스 정의
 interface ChatMessage {
@@ -22,7 +22,9 @@ export default function ChatRoom() {
   // WebSocket 연결 설정
   useEffect(() => {
     const socket = new SockJS("http://localhost:4000/ws"); // 서버의 WebSocket 엔드포인트에 연결
-    const stompClient = Stomp.over(socket); // STOMP를 사용해 SockJS로 WebSocket 연결
+    const stompClient = Stomp.over(() => socket); // STOMP를 사용해 SockJS로 WebSocket 연결
+    console.log(socket);
+    console.log(stompClient);
 
     console.log("0");
     // STOMP 클라이언트 연결
@@ -30,18 +32,19 @@ export default function ChatRoom() {
       {},
       () => {
         console.log("Connected to WebSocket");
-        console.log("1");
         // 특정 채팅방(roomId)에 대한 구독 설정
-        stompClient.subscribe(`/topic/${roomId}`, (msg: any) => {
-          const receivedMessage: ChatMessage = JSON.parse(msg.body); // 수신한 메시지를 파싱
-          setMessages((prevMessages) => [...prevMessages, receivedMessage]); // 수신한 메시지를 메시지 배열에 추가
-        });
+        // stompClient.subscribe(`/topic/${roomId}`, (msg: any) => {
+        //   const receivedMessage: ChatMessage = JSON.parse(msg.body); // 수신한 메시지를 파싱
+        //   setMessages((prevMessages) => [...prevMessages, receivedMessage]); // 수신한 메시지를 메시지 배열에 추가
+        // });
       },
-      (error) => {
+      (error: any) => {
         console.error("WebSocket connection error:", error);
       }
     );
+
     setStompClient(stompClient); // STOMP 클라이언트를 상태로 저장
+
     // 컴포넌트 언마운트 시 WebSocket 연결 해제
     return () => {
       console.log("4");
